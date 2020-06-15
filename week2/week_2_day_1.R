51011ee31d02a61bf1a6c8642050298a354360d2
Iman Abakoyas <imanj.kadi@gmail.com>
2020-06-14 23:43:44 -0400

    Adding exercise day 1 and 2

--- week2/week_2_day_1.R
+++ week2/week_2_day_1.R
@@ -124,8 +124,8 @@ mean(magnets$change)
 # Factor
 
 # 3. Compute the average value of the variable “change” for the patients that received and active magnet and average value for those that received an inactive placebo. 
-#(Hint: Notice that the ﬁrst 29 patients received an active magnet and the last 21 patients received an inactive placebo. The subsequence of the ﬁrst 29 values of the 
-#ngiven variables can be obtained via the expression “change[1:29]” and the last 21 vales are obtained via the expression “change[30:50]”.)
+# (Hint: Notice that the ﬁrst 29 patients received an active magnet and the last 21 patients received an inactive placebo. The subsequence of the ﬁrst 29 values of the 
+# ngiven variables can be obtained via the expression “change[1:29]” and the last 21 vales are obtained via the expression “change[30:50]”.)
 mean(magnets$change[1:29])
 # 5.241379
 
@@ -145,3 +145,86 @@ boxplot(magnets$change[1:29])
 
 boxplot(magnets$change[30:50])
 # 3 outliers
+
+10.6 Exercises
+# Question 10.1. In Subsection 10.3.2 we compare the average against the midrange as estimators of the expectation of the measurement. The goal of this exercise is to repeat the analysis, but this time
+ compare the average to the median as estimators of the expectation in symmetric distributions.
+
+# 1. Simulate the sampling distribution of average and the median of a sample of size n = 100 from the Normal(3,2) distribution. Compute the expectation and the variance of the sample average and of the 
+sample median. Which of the two estimators has a smaller mean square error?
+samp _mean<- rep(0,10^5)  
+samp_median <- rep(0,10^5)
+for(i in 1:10^5)  
+    x <- rnorm(100,mu,sig) 
+    samp_meam=n[i] <- mean(X) 
+    samp_median[i] <- median(X) 
+
+mean(samp_mean)
+# 2.999803
+mean(samp_median)
+# 3.000807
+var(samp_mean)
+# 0.03996177
+var(sample_median)
+# 0.06196287
+
+# 2. Simulate the sampling distribution of average and the median of a sample of size n = 100 from the Uniform(0.5,5.5) distribution. Compute the expectation and the variance of the sample average and 
+of the sample median. Which of the two estimators has a smaller mean square error?
+samp _mean<- rep(0,10^5)  
+samp_median <- rep(0,10^5)
+for(i in 1:10^5)
+    x <- runif(100,0.5,5.5)
+    samp_mean[i] <- mean(x) 
+    samp_median[i] <- median(x)
+
+mean(samp_mean)
+# 3.000397
+mean(samp_median)
+# 3.000712
+var(samp_mean)
+# 0.02078071
+var(sample_median)
+# 0.06056054
+
+# Question 10.2. The goal in this exercise is to assess estimation of a proportion in a 
+population on the basis of the proportion in the sample. The ﬁle “pop2.csv” was introduced 
+in Exercise 7.1 of Chapter 7. This ﬁle contains information associated to the blood pressure 
+of an imaginary population of size 100,000. The ﬁle can be found on the internet 
+(http://pluto. huji.ac.il/~msby/StatThink/Datasets/pop2.csv). One of the variables in the ﬁle
+is a factor by the name “group” that identiﬁes levels of blood pressure. The levels of this 
+variable are “HIGH”, “LOW”, and “NORMAL”. The ﬁle “ex2.csv” contains a sample of size n = 150
+taken from the given population. This ﬁle can also be found on the internet 
+(http://pluto.huji. ac.il/~msby/StatThink/Datasets/ex2.csv). It contains the same variables as 
+in the ﬁle “pop2.csv”. The ﬁle “ex2.csv” corresponds in this exercise to the observed sample 
+and the ﬁle “pop2.csv” corresponds to the unobserved population. Download both ﬁles to your 
+computer and answer the following questions:
+
+# 1. Compute the proportion in the sample of those with a high level of blood pressure16.
+37/150  
+# 0.2466667
+
+2. Compute the proportion in the population of those with a high level of blood pressure.
+
+
+
+
+
+# 3.  Simulate the sampling distribution of the sample proportion and compute its expectation.
+a <- rep(0,10^5) 
+for(i in 1:10^5)  
+    x <- sample(pop2$group,150) 
+    a[i] <- mean(X == "HIGH")
+ 
+mean(a)
+# 0.281230
+
+# 4. Compute the variance of the sample proportion.
+var(a)
+#  0.001350041
+
+# 5. It is proposed in Section 10.5 that the variance of the sample proportion is 
+Var( ˆ P) = p(1−p)/n, where p is the probability of the event (having a high blood pressure in our case)
+and n is the sample size (n = 150 in our case). Examine this proposal in the current setting.
+b <- mean(pop2$group == "HIGH") 
+b*(1-p)/150 
+# 0.001347685
